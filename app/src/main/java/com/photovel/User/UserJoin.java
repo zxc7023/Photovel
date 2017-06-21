00e90b2633bd86db4a9353ad0926fb9f14ffc80e
zxc7023 <zxc7023@gmail.com>
2017-06-21 18:09:40 +0900

    ㅎㅎ

--- app/src/main/java/com/photovel/User/UserJoin.java
+++ app/src/main/java/com/photovel/User/UserJoin.java
@@ -14,6 +14,5 @@ public class UserJoin extends FontActivity2 {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.);
     }
 }
