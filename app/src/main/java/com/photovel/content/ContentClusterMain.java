d0b55b28e3ac94a054c8e5061de37cb05f462ec6
EUNDI\Eundi <leeej9201@gmail.com>
2017-06-26 23:17:38 +0900

    170626 cluster content_id별로 사진 나오게 수정

--- app/src/main/java/com/photovel/content/ContentClusterMain.java
+++ app/src/main/java/com/photovel/content/ContentClusterMain.java
@@ -5,7 +5,11 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.Typeface;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
@@ -24,6 +28,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -32,7 +37,22 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.alibaba.fastjson.JSON;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.clustering.Cluster;
+import com.google.maps.android.clustering.ClusterItem;
+import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.clustering.view.DefaultClusterRenderer;
+import com.google.maps.android.ui.IconGenerator;
 import com.photovel.FontActivity2;
+import com.photovel.FontActivity3;
 import com.photovel.MainActivity;
 import com.photovel.R;
 import com.photovel.http.Value;
@@ -52,9 +72,11 @@ import java.net.ProtocolException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class ContentClusterMain extends FontActivity2 implements NavigationView.OnNavigationItemSelectedListener {
+public class ContentClusterMain extends FontActivity3 implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback, ClusterManager.OnClusterClickListener<Photo>,
+        ClusterManager.OnClusterInfoWindowClickListener<Photo>, ClusterManager.OnClusterItemClickListener<Photo>, ClusterManager.OnClusterItemInfoWindowClickListener<Photo> {
     private SearchView searchView;
     private static final String TAG = "AppPermission";
     Toolbar toolbar;
@@ -75,10 +97,20 @@ public class ContentClusterMain extends FontActivity2 implements NavigationView.
     private final String contentURL = Value.contentURL;
     private final String contentPhotoURL = Value.contentPhotoURL;
 
+    //은지 수정
+    private ClusterManager<Photo> cm;
+    private GoogleMap mMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content_cluster_main);
+
+        //setContentView(R.layout.activity_content_cluster);
+
+        //프래그먼트에 지도를 보여주기위해 싱크
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
         Intent intent = getIntent();
         content_id = intent.getIntExtra("content_id",1);
@@ -89,8 +121,8 @@ public class ContentClusterMain extends FontActivity2 implements NavigationView.
         }
 
         // Adding Toolbar to the activity
-        toolbar = (Toolbar) findViewById(R.id.clusterToolbar);
-        setSupportActionBar(toolbar);
+        /*toolbar = (Toolbar) findViewById(R.id.clusterToolbar);
+        setSupportActionBar(toolbar);*/
 
         icglobe = (TextView)findViewById(R.id.icglobe);
         icleft = (TextView)findViewById(R.id.icleft);
@@ -190,6 +222,7 @@ public class ContentClusterMain extends FontActivity2 implements NavigationView.
             }
             myDataset.add(content.getDetails().get(i));
         }
+
         tvContentLocation.setText(content.getDetails().get(content.getDetails().size()-1).getPhoto().getAddress());
         String from = new SimpleDateFormat("yyyy.MM.dd").format(content.getDetails().get(0).getPhoto().getPhoto_date());
         String to = new SimpleDateFormat("yyyy.MM.dd").format(content.getDetails().get(content.getDetails().size()-1).getPhoto().getPhoto_date());
@@ -223,6 +256,7 @@ public class ContentClusterMain extends FontActivity2 implements NavigationView.
                 nsv.fullScroll(View.FOCUS_UP);
             }
         });
+
     }
 
     //onClick
@@ -596,6 +630,187 @@ public class ContentClusterMain extends FontActivity2 implements NavigationView.
             drawer.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.50111, -0.122777775), 9.5f));
+        startDemo();
+    }
+
+    @Override
+    public boolean onClusterClick(Cluster<Photo> cluster) {
+        // Show a toast with some info when the cluster is clicked.
+
+        String firstName = cluster.getItems().iterator().next().getPhoto_file_name();
+        Toast.makeText(this, cluster.getSize() + " (including " + firstName + ")", Toast.LENGTH_SHORT).show();
+
+        // Zoom in the cluster. Need to create LatLngBounds and including all the cluster items
+        // inside of bounds, then animate to center of the bounds.
+
+        // Create the builder to collect all essential cluster items for the bounds.
+        LatLngBounds.Builder builder = LatLngBounds.builder();
+        for (ClusterItem item : cluster.getItems()) {
+            builder.include(item.getPosition());
+        }
+        // Get the LatLngBounds
+        final LatLngBounds bounds = builder.build();
+
+        // Animate camera to the bounds
+        try {
+            mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(bounds, 100));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return true;
+    }
+
+    protected void startDemo() {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myDataset.get(0).getPhoto().getPhoto_latitude(), myDataset.get(0).getPhoto().getPhoto_longitude()), 12.0f));
+
+        cm = new ClusterManager<Photo>(this, mMap);
+        cm.setRenderer(new MarkerRenderer());
+
+        mMap.setOnCameraIdleListener(cm);
+        //아이콘(마커) 클릭 인식
+        mMap.setOnMarkerClickListener(cm);
+        mMap.setOnInfoWindowClickListener(cm);
+
+        cm.setOnClusterClickListener(this);
+        cm.setOnClusterInfoWindowClickListener(this);
+        cm.setOnClusterItemClickListener(this);
+        cm.setOnClusterItemInfoWindowClickListener(this);
+        for (int i = 0; i < myDataset.size(); i++) {
+            cm.addItem(myDataset.get(i).getPhoto());
+        }
+        cm.cluster();
+
+    }
+
+    @Override
+    public void onClusterInfoWindowClick(Cluster<Photo> cluster) {
+
+    }
+
+    @Override
+    public boolean onClusterItemClick(final Photo photo) {
+        // Does nothing, but you could go into the user's profile page, for example.
+        Toast.makeText(this, photo.getPhoto_file_name() + "이 선택되었습니다.", Toast.LENGTH_SHORT).show();
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
+            }
+
+            @Override
+            public View getInfoContents(Marker marker) {
+                View testView = getLayoutInflater().inflate(R.layout.multi_photo_detail, null);
+                ((ImageView) testView.findViewById(R.id.photo_detail)).setImageBitmap(photo.getBitmap());
+                return testView;
+            }
+        });
+        return false;
+    }
+
+    @Override
+    public void onClusterItemInfoWindowClick(Photo photo) {
+
+    }
+
+    private class MarkerRenderer extends DefaultClusterRenderer<Photo> {
+        private final IconGenerator mIconGenerator = new IconGenerator(getApplicationContext()); //마커의 아이콘을 Generator해줌
+        private final IconGenerator mClusterIconGenerator = new IconGenerator(getApplicationContext()); // 클러스트된 아이콘을 Generator해줌
+        private final ImageView mImageView; // 클러스트가 되어진 마커가 아닌 일반 마커를 의미
+        private final ImageView mClusterImageView; // 클러스트가 된 마커를 의미
+
+        private final int mDimension;
+        View multiPhotoView;
+        ImageView proFileInImageView;
+        TextView rankTextView;
+        TextView amu_text;
+
+
+        public MarkerRenderer() {
+            super(getApplicationContext(), mMap, cm);
+            Drawable TRANSPARENT_DRAWABLE = new ColorDrawable(Color.TRANSPARENT);
+            multiPhotoView = getLayoutInflater().inflate(R.layout.multi_photo, null);
+            multiPhotoView.findViewById(R.id.rankTextView).setVisibility(View.GONE);
+            mClusterIconGenerator.setContentView(multiPhotoView); // 인플레이터한 전체레이아웃을 아이콘으로 만들어준다.
+            mClusterImageView = (ImageView) multiPhotoView.findViewById(R.id.image); //multiPhotoView안의 이미지뷰를 찾아줌
+
+            //새로 만든 이미지뷰를 설정해줌
+            mImageView = new ImageView(getApplicationContext());
+            mDimension = (int) getResources().getDimension(R.dimen.custom_profile_image);
+            mImageView.setLayoutParams(new ViewGroup.LayoutParams(mDimension, mDimension));
+            int padding = (int) getResources().getDimension(R.dimen.custom_profile_padding);
+            mImageView.setPadding(padding, padding, padding, padding);
+            mIconGenerator.setContentView(mImageView);
+
+            mIconGenerator.setBackground(TRANSPARENT_DRAWABLE);
+        }
+
+        @Override
+        protected void onBeforeClusterItemRendered(Photo photo, MarkerOptions markerOptions) {
+            // Draw a single person.
+            // Set the info window to show their name.
+            /*
+            원본입니다.
+            mImageView.setImageBitmap(photo.getPhoto());
+            Bitmap icon = mIconGenerator.makeIcon();
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon)).title(photo.name);
+            */
+            View newMultiPhotoView = getLayoutInflater().inflate(R.layout.multi_photo, null);
+            //시도중입니다.
+            proFileInImageView = (ImageView) newMultiPhotoView.findViewById(R.id.image);
+            proFileInImageView.setImageBitmap(photo.getBitmap());
+            rankTextView = (TextView) newMultiPhotoView.findViewById(R.id.rankTextView);
+//            rankTextView.setText(photo.getRank()+"");
+            amu_text = (TextView) newMultiPhotoView.findViewById(R.id.amu_text);
+            amu_text.setVisibility(View.GONE);
+
+            mIconGenerator.setContentView(newMultiPhotoView);
+            Bitmap icon = mIconGenerator.makeIcon();
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon));
+            //.title(photo.getName());
+
+
+        }
+
+        @Override
+        protected void onBeforeClusterRendered(Cluster<Photo> cluster, MarkerOptions markerOptions) {
+            // Draw multiple people.
+            // Note: this method runs on the UI thread. Don't spend too much time in here (like in this example).
+            List<Drawable> photos = new ArrayList<Drawable>(Math.min(4, cluster.getSize()));
+            int width = mDimension;
+            int height = mDimension;
+
+            for (Photo p : cluster.getItems()) {
+                // Draw 4 at most.
+                if (photos.size() == 4) break;
+                Drawable bitmapDrawable = new BitmapDrawable(getResources(), p.getBitmap());
+                bitmapDrawable.setBounds(0, 0, width, height);
+                photos.add(bitmapDrawable);
+            }
+            MultiDrawable multiDrawable = new MultiDrawable(photos);
+            multiDrawable.setBounds(0, 0, width, height);
+
+
+/*            for(Bitmap bitmap :profilePhotos){
+                mClusterImageView.setImageBitmap(bitmap);
+            }*/
+            mClusterImageView.setImageDrawable(multiDrawable);
+            Bitmap icon = mClusterIconGenerator.makeIcon(String.valueOf(cluster.getSize()));
+            markerOptions.icon(BitmapDescriptorFactory.fromBitmap(icon));
+
+        }
+
+        @Override
+        protected boolean shouldRenderAsCluster(Cluster cluster) {
+            // Always render clusters.
+            return cluster.getSize() > 1;
         }
     }
 }
